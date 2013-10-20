91356b89bf5f374c7b6f88458d9f2dcc8f06e2be
Ernest Micklei <ernest.micklei@gmail.com>
2013-10-20 21:35:23 +0200

    fetching and unmarshalling the apis

---
+++ tools/restful-client-generator.go
@@ -0,0 +1,57 @@
+package main
+
+// Takes a Swagger JSON file and generates a Service Http client in Go
+
+import (
+	"encoding/json"
+	"flag"
+	"github.com/emicklei/go-restful/swagger"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+var apidocsUrl string
+
+func main() {
+	flag.StringVar(&apidocsUrl, "url", "", "endpoint of a REST service (e.g. http://myservice/apidocs.json)")
+	flag.Parse()
+	if len(apidocsUrl) == 0 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	log.Print("fetching Swagger docs")
+
+	listing := new(swagger.ResourceListing)
+	fetch(apidocsUrl, &listing)
+	log.Printf("Api version:%s, Swagger version:%s", listing.ApiVersion, listing.SwaggerVersion)
+
+	for _, each := range listing.Apis {
+		generateForApi(each)
+	}
+}
+
+func generateForApi(api swagger.Api) {
+	declaration := new(swagger.ApiDeclaration)
+	fetch(apidocsUrl+api.Path, &declaration)
+	for _, each := range declaration.Apis {
+		log.Printf("api:%v", each.Path)
+		for _, op := range each.Operations {
+			log.Printf("operation:%v", op.HttpMethod)
+		}
+	}
+}
+
+func fetch(url string, model interface{}) {
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatalf("Unable to fetch Swagger specification:%v", err)
+	}
+	buffer, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Unable to read Swagger specification:%v", err)
+	}
+	json.Unmarshal(buffer, model)
+}
